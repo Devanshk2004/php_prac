a7065cf0184490b8f3f9e8a33500dbcfd7c26762
Devanshk2004 <devansh.kalura55@gmail.com>
2026-02-03 07:02:16 +0530

    signup

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php 
+require_once 'includes/config_session.inc.php';
+require_once 'includes/signup_view.inc.php'
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -23,11 +28,14 @@
     <h3>Signup</h3>
 
     <form action="includes/signup.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="pwd" placeholder="Password">
-        <input type="text" name="email" placeholder="E-Mail">
+        <?php signup_inputs();
+        ?>
         <button>Signup</button>
     </form>
+
+    <?php
+    check_signup_erros();
+    ?>
 
 </body>
 
