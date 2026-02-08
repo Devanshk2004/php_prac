5812a7817db55a8229bf502cebfa7ecb1fb64a7f
Devanshk2004 <devansh.kalura55@gmail.com>
2026-02-08 20:14:11 +0530

    login-done

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 require_once 'includes/config_session.inc.php';
-require_once 'includes/signup_view.inc.php'
+require_once 'includes/signup_view.inc.php';
+require_once 'includes/login_view.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -17,13 +18,26 @@ require_once 'includes/signup_view.inc.php'
 
 <body>
 
-    <h3>Login</h3>
+    <h3>
+        <?php
+        output_username();
+        ?>
+    </h3>
 
-    <form action="includes/login.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="pwd" placeholder="Password">
-        <button>Login</button>
-    </form>
+    <?php
+    if (!isset($_SESSION["user_id"])){ ?>
+        <h3>Login</h3>
+
+        <form action="includes/login.inc.php" method="post">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="pwd" placeholder="Password">
+            <button>Login</button>
+        </form>
+    <?php } ?>
+    
+    <?php
+    check_login_errors();
+    ?>
 
     <h3>Signup</h3>
 
@@ -36,6 +50,12 @@ require_once 'includes/signup_view.inc.php'
     <?php
     check_signup_erros();
     ?>
+
+    <h3>Logout</h3>
+
+    <form action="includes/logout.inc.php" method="post">
+        <button>Logout</button>
+    </form>
 
 </body>
 
